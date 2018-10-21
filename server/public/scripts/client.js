c9be2f5940f55bac3a6c2f3fadfb478620690fa3
Malik-G <Malik-G@users.noreply.github.com>
2018-10-21 01:53:04 -0500

    updated to include DELETE and PUT functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -12,6 +12,19 @@ function readyNow(){
    $('#addButton').on('click', addBook);
    $('#bookTable').on('click', '.deleteBtn', deleteBook);
    $('#bookTable').on('click', '.editBtn', editBook);
+   $('#bookTable').on('click', '.acceptBtn', acceptEdits);
+}
+
+function getCollection(){
+  $.ajax({
+     method: 'GET',
+     url: '/books',
+  }).then( function(response) {
+     console.log('Data successfully received from server/database', response);
+     appendBookData(response);
+  }).catch( function(response) {
+     console.log('Error receiving data from server/database');
+  })
 }
 
 function addBook(event){
@@ -33,39 +46,6 @@ function addBook(event){
    })
 }
 
-function getInputs(){
-   newBook.title = $('#titleInput').val();
-   newBook.author = $('#authorInput').val();
-   newBook.published = $('#dateInput').val();
-}
-
-function getCollection(){
-   $.ajax({
-      method: 'GET',
-      url: '/books',
-   }).then( function(response) {
-      console.log('Data successfully received from server/database', response);
-      appendBookData(response);
-   }).catch( function(response) {
-      console.log('Error receiving data from server/database');
-   })
-}
-
-function appendBookData(arrOfObjs){
-   $('#bookTable').empty();
-   for(let obj of arrOfObjs){
-     let newRow = $(`<tr>
-      <td>${obj.title} <br> <input class="hide" type="text"> </td>
-      <td>${obj.author} <br> <input class="hide" type="text"> </td>
-      <td>${obj.published.substring(0,10)} <br> <input class="hide" type="text"> </td>
-      <td><button class="deleteBtn">Delete</button>
-      <button class="editBtn">Edit</button></td>
-      <tr`);
-      $('#bookTable').append(newRow);
-      newRow.data('book_id', obj.book_id);
-   }
-}
-
 function deleteBook(){
    let rowClicked = $(this).closest('tr');
    let bookId = rowClicked.data('book_id');
@@ -75,11 +55,63 @@ function deleteBook(){
    }).then(function(response){
       console.log('Successfully deleted from database');
       getCollection();
-   })
+   }).catch(function(error) {
+      console.log('Error response from database:', error);
+  })
 }
 
 function editBook(){
-   let rowClicked = $(this).closest('tr');
-   rowClicked.find('input').toggleClass('hide');
-   
+  let rowClicked = $(this).closest('tr');
+  toggleEditFields(rowClicked);
+}
+
+function acceptEdits(){
+  console.log('In makeEdits()');
+  let rowClicked = $(this).closest('tr');
+  let bookId = rowClicked.data('book_id');
+  newBook.title = rowClicked.find('.input1').val();
+  newBook.author = rowClicked.find('.input2').val();
+  newBook.published = rowClicked.find('.input3').val();
+  console.log('New edits:', newBook.title, '-', newBook.author, '-', newBook.published);
+  $.ajax({
+    method: 'PUT',
+    url:`/books/update/${bookId}`,
+    data:{
+      title: newBook.title,
+      author: newBook.author,
+      published: newBook.published
+    }
+  }).then(function(response){
+    console.log('Response from the sever:', response);
+    getCollection();
+  }).catch(function(error){
+    console.log('Error updating database entry:', error);
+  })
+}
+
+function getInputs(){
+  newBook.title = $('#titleInput').val();
+  newBook.author = $('#authorInput').val();
+  newBook.published = $('#dateInput').val();
+}
+
+function appendBookData(arrOfObjs){
+  $('#bookTable').empty();
+  for(let obj of arrOfObjs){
+    let newRow = $(`<tr>
+     <td>${obj.title} <br> <input class="input1 hide" type="text"> </td>
+     <td>${obj.author} <br> <input class="input2 hide" type="text"> </td>
+     <td>${obj.published.substring(0,10)} <br> <input class="input3 hide" type="text"> </td>
+     <td><button class="deleteBtn">Delete</button>
+     <button class="editBtn">Edit</button>
+     <button class="acceptBtn hide">Accept</button></td>
+     <tr`);
+     $('#bookTable').append(newRow);
+     newRow.data('book_id', obj.book_id);
+  }
+}
+
+function toggleEditFields(targetRow){
+  targetRow.find('input').toggleClass('hide');
+  targetRow.find('.acceptBtn').toggleClass('hide');
 }
--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ if (process.env.DATABASE_URL) {
   const params = url.parse(process.env.DATABASE_URL);
   const auth = params.auth.split(':');
 
-  config = {
+  config = { // runs on heroku server
     user: auth[0],
     password: auth[1],
     host: params.hostname,
@@ -28,60 +28,61 @@ if (process.env.DATABASE_URL) {
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
 } else {
-  config = {
+  config = { // runs on local server
     host: 'localhost', // Server hosting the postgres database
     port: 5432, // env var: PGPORT
-    database: process.env.DATABASE_NAME || 'bookstore', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    database: process.env.DATABASE_NAME || 'bookstore', // this is likely the one thing that needs to be changed
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
 }
 
-const pool = new pg.Pool(config);
 //const Pool = pg.Pool;
+const pool = new pg.Pool(config);
 
 // Tests the connection
 pool.on('connect', () => {
    console.log('Connected to the database');
- })
- 
- pool.on('error', (error) => {
+})
+pool.on('error', (error) => {
    console.log('Error with database pool', error);
- })
+})
+
+// ROUTES
 
 app.get('/books', (req, res) => {
-   const sqlText = 'SELECT * FROM books ORDER BY author, title'
-   pool.query(sqlText)
-      .then((result) =>{
-         console.log(result);
-         res.send(result.rows);
-      })
-      .catch((error) =>{
-         console.log('Error making query');
-         res.sendStatus(500);
-      })
+  const sqlText = 'SELECT * FROM books ORDER BY author, title'
+  pool.query(sqlText)
+    .then((result) =>{
+        console.log(result);
+        res.send(result.rows);
+    })
+    .catch((error) =>{
+        console.log('Error making query');
+        res.sendStatus(500);
+    })
 })
 
 app.post('/books', (req, res) => {
-   const newBook = req.body;
-   const sqlText = `INSERT INTO books (title, author, published) VALUES 
-   ($1, $2, $3)`;
-   // Let sql sanitize your inputs (NO Bobby Drop Tables here!)
-   // the $1, $2, etc get substituted with the values from the array below
-   pool.query(sqlText, [newBook.title, newBook.author, newBook.published])
-     .then( (result) => {
-       console.log(`Added song to the database`, newBook);
-       res.sendStatus(201);
-     })
-     .catch( (error) => {
-       console.log(`Error making database query ${sqlText}`, error);
-       res.sendStatus(500); // Good server always responds
-     })
- })
+  const newBook = req.body;
+  const sqlText = `INSERT INTO books (title, author, published) VALUES 
+  ($1, $2, $3)`;
+  // Let sql sanitize your inputs (NO Bobby Drop Tables here!)
+  // the $1, $2, etc get substituted with the values from the array below
+  pool.query(sqlText, [newBook.title, newBook.author, newBook.published])
+    .then( (result) => {
+      console.log(`Added song to the database`, newBook);
+      res.sendStatus(201);
+    })
+    .catch( (error) => {
+      console.log(`Error making database query ${sqlText}`, error);
+      res.sendStatus(500); // Good server always responds
+    })
+})
 
  // req.params refers to the params specified in the the url template,
  // so in this case :id is the param, therefore use req.params.id
- app.delete('/books/:id', (req, res) => {
+app.delete('/books/:id', (req, res) => {
   let reqId = req.params.id;
   let sqlText = 'DELETE FROM books WHERE book_id=$1';
   pool.query(sqlText, [reqId])
@@ -93,8 +94,22 @@ app.post('/books', (req, res) => {
       console.log(`Error deleting`);
       res.sendStatus(500); // Good server always responds
     })
- })
+})
 
- app.listen( port, () => {
+app.put('/books/update/:id', (req, res) => {
+  let reqId = req.params.id;
+  let dataObj = req.body;
+  let sqlText = `UPDATE books SET title=$1, author=$2, published=$3 WHERE book_id=$4`
+  pool.query(sqlText, [dataObj.title, dataObj.author, dataObj.published, reqId])
+    .then((result) => {
+      console.log('Updated successfully! New data:', result);
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+    })
+})
+
+app.listen( port, () => {
   console.log('up and running on port ', port);
 });
